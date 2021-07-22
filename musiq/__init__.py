2014d187dc2c98cb6d6ff6839e09cb0ba969e651
Sitdhibong Laokok <sitdhibong@gmail.com>
2021-07-22 16:16:44 +0700

    Prepare for services

--- musiq/__init__.py
+++ musiq/__init__.py
@@ -1,6 +1,8 @@
+import json
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, make_response, request
 from flask_sqlalchemy import SQLAlchemy
+from requests import models
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_mapping(
@@ -54,3 +56,60 @@ def index():
     songs = json.load(f)
     
   return render_template('index.html', songs=songs)
+
+@app.route('/api/artist/next')
+def next_artist():
+  from models import ArtistMap, Song
+  artist_list = [a.artist_name_spotify.strip() for a in ArtistMap.query.with_entities(ArtistMap.artist_name_spotify).distinct()]
+
+  song_artist = [a.artist for a in Song.query.filter(~Song.artist.in_(artist_list)).limit(1)]
+  return make_response(jsonify({
+    'artist': None if 0 == len(song_artist) else song_artist[0],
+  }), 200)
+
+@app.route('/api/artist/search/<string:q>')
+def search_artist(q):
+  from models import SiamzoneSong, Song
+
+  artist_result = SiamzoneSong.query.with_entities(SiamzoneSong.artist, SiamzoneSong.artist_english).distinct().filter(SiamzoneSong.artist.like(f'%{q}%'))
+  app.logger.debug(artist_result)
+  artists = []
+  for a in artist_result:
+    artists.append({
+      'artist_thai': a.artist,
+      'artist_english': a.artist_english,
+    })
+
+  return make_response(
+    jsonify(artists),
+    200 if len(artists) > 0 else 404
+  )
+
+@app.route('/api/map/artist', methods=['POST'])
+def map_artist():
+  from flask import request
+  from models import ArtistMap
+
+  content = request.json
+
+  spotify_artist = content.get('spotify_artist')
+  known_artists = content.get('mapped_artists')
+
+  if 0 == len(known_artists):
+    app.logger.debug(f'Empty artist for {spotify_artist}')
+    db.session.add(ArtistMap(
+      artist_name_spotify=spotify_artist,
+      artist_name_siamzone=None
+    ))
+
+  else:
+    app.logger.debug(f'Updating list of artist for {spotify_artist}')
+    for artist in known_artists:
+      db.session.add(ArtistMap(
+        artist_name_spotify=spotify_artist,
+        artist_name_siamzone=artist
+      ))
+
+  db.session.commit()
+
+  return make_response(jsonify(content), 200)
